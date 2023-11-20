29c08c843433ebba2a02db90901fbb4c3aaf9cb6
capy-git-bara <150447077+capy-git-bara@users.noreply.github.com>
2023-11-20 14:48:26 +0900

    energykWh added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 def main():
-    st.title("Peak Waiters App for Australia")
+    st.title("Peak Waiters and Energy App for Australia")
 
     uploaded_file = st.file_uploader("Upload your .xlsx file")
 
@@ -17,15 +17,18 @@ def main():
         if selected_public_names:
             filtered_df = df[df['public_name'].isin(selected_public_names)]
 
-            # Plotly를 사용하여 바 그래프 생성
-            fig = px.bar(filtered_df, x='event_dt', y='peak_waiters', color='public_name', title="Peak Waiters for Selected Sites")
-            fig.update_xaxes(title="Date", dtick="D", tickformat="%b %d, %Y")
-            fig.update_yaxes(title="Peak Waiters")
+            # Plotly를 사용하여 바 차트 생성
+            bar_chart = px.bar(filtered_df, x='event_dt', y='peak_waiters', color='public_name', title="Peak Waiters for Selected Sites")
+            bar_chart.update_xaxes(title="Date", dtick="D", tickformat="%b %d, %Y")
+            bar_chart.update_yaxes(title="Peak Waiters")
+            bar_chart.add_trace(go.Scatter(x=filtered_df['event_dt'], y=[10]*len(filtered_df), mode='lines', name='Threshold', line=dict(color='red', dash='dot')))
+            st.plotly_chart(bar_chart, use_container_width=True)
 
-            # y = 10인 빨간색 점선 추가
-            fig.add_trace(go.Scatter(x=filtered_df['event_dt'], y=[10]*len(filtered_df), mode='lines', name='Threshold', line=dict(color='red', dash='dot')))
-
-            st.plotly_chart(fig, use_container_width=True)
+            # Plotly를 사용하여 라인 차트 생성
+            line_chart = px.line(filtered_df, x='event_dt', y='peak_energy', color='public_name', title="Peak Energy for Selected Sites")
+            line_chart.update_xaxes(title="Date", dtick="D", tickformat="%b %d, %Y")
+            line_chart.update_yaxes(title="Peak Energy (kWh)")
+            st.plotly_chart(line_chart, use_container_width=True)
 
 if __name__ == "__main__":
     main()
